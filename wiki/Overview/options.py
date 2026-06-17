54eb64c764932e11901df529b5eec547d00bc180
Audrey Plunkett <alphacataudrey@gmail.com>
2026-06-17 10:51:41 -0500

    Changed all options.py files to options.yaml files

--- wiki/Overview/options.py
+++
@@ -1,1 +0,0 @@
-options = {'dropdown': False, 'order': ['*'], 'title_level': 0, 'ordered': False}
--- wiki/options.py
+++
@@ -1,1 +0,0 @@
-options = {'dropdown': False, 'order': ['Overview','Reference_Manual','Index','Glossary','Tutorials'], 'title_level': 0, 'ordered': True}
